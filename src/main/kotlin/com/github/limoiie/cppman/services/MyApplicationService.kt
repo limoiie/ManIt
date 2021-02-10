fa9bf96a9fd734ef56bab1641e66ba81c6f20703
GitHub Action <action@github.com>
2021-02-10 13:38:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/limoiie/cppman/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/limoiie/cppman/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.limoiie.cppman.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.limoiie.cppman.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/limoiie/cppman/services/MyProjectService.kt
+++ src/main/kotlin/com/github/limoiie/cppman/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.limoiie.cppman.services
 
+import com.github.limoiie.cppman.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
