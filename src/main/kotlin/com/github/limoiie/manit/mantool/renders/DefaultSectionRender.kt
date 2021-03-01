d0d14dc6896439a27b8ce2cb9136002171f7da16
limoiie <limo.iie4@gmail.com>
2021-03-01 09:29:32 +0800

    refactor: Remove duplicated section header

--- src/main/kotlin/com/github/limoiie/manit/mantool/renders/DefaultSectionRender.kt
+++ src/main/kotlin/com/github/limoiie/manit/mantool/renders/DefaultSectionRender.kt
@@ -2,7 +2,7 @@ package com.github.limoiie.manit.mantool.renders
 
 class DefaultSectionRender(private val lineRender: LineRender) : SectionRender {
     override fun render(section: String?, lines: List<String>): String {
-        val content = lineRender.firstRender() + renderLines(lines) +
+        val content = lineRender.firstRender() + renderLines(lines.drop(1)) +
                 lineRender.lastRender()
         return """<div class="section">$section</div>""" + LINE_END +
                 """<div style="margin-left:20px">$content</div>"""
